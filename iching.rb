80180eb244c14c56b1a737af7ecce8a674e48cc2
Alfonso Adriasola <aadriasola@cloudpassage.com>
2017-08-08 15:38:46 -0700

    initial commit
    hexgarams from dan enright
    ruby script from alfonso adriasola

---
+++ iching.rb
@@ -0,0 +1,79 @@
+class Iching
+  def cast(maker: CoinMethodHexagramMaker, changer: HexagramChanger, renderer: HexagramRenderer)
+    puts "#{ARGV[0]}"
+
+    hexagram = maker.new.do
+    hexagram2 = changer.new.do hexagram
+
+    renderer.new.do hexagram
+    renderer.new.do hexagram2
+  end
+end
+
+class CoinMethodHexagramMaker
+  def do
+    hexagram = []
+    6.times do
+      sleep(0.2)
+      throw = 3.times.map { Random.new.rand(2); sleep(0.2) }
+      hexagram << throw.map { |c| c == 1 ? 3 : 2 }.inject(0, :+)
+    end
+    hexagram
+  end
+end
+
+class HexagramChanger
+  def do(hexagram)
+    hexagram.map do |l|
+      case l
+        when 9
+          7
+        when 8
+          8
+        when 7
+          7
+        when 6
+          8
+      end
+    end
+  end
+end
+
+class HexagramRenderer
+  def link(hexagram)
+    linkaddr = hexagram.map do |l|
+      case l.to_i
+        when 9
+          0
+        when 8
+          0
+        when 7
+          1
+        when 6
+          1
+      end
+    end.reverse
+    puts
+    puts " file:///Users/aadriasola/Documents/IChing/hexagrams/#{linkaddr.join}.html"
+    puts
+  end
+
+  def do(hexagram)
+    hexagram.reverse.map do |l|
+      case l.to_i
+        when 9
+          puts '---~  ~---'
+        when 8
+          puts '--      --'
+        when 7
+          puts '----------'
+        when 6
+          puts '---~~~~---'
+      end
+    end
+    link(hexagram)
+  end
+end
+
+
+Iching.new.cast
