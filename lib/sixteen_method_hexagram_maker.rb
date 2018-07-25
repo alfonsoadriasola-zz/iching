38e2c80e1198c7bebf500ba4f7d8f7461aa1c921
Alfonso Adriasola <aadriasola@cloudpassage.com>
2018-07-25 08:59:06 -0700

    that didnt go well - i meant this

--- lib/sixteen_method_hexagram_maker.rb
+++ lib/sixteen_method_hexagram_maker.rb
@@ -3,6 +3,7 @@ class SixteenMethodHexagramMaker
   MAP = { old_yin: 6, young_yang: 7, young_yin: 8, old_yang: 9 }.freeze
 
   def do
+    Random.srand Time.now.to_i
     marbles = []
     marbles += [:old_yin]
     marbles += Array.new(5) { :young_yang }
@@ -10,8 +11,7 @@ class SixteenMethodHexagramMaker
     marbles += Array.new(3) { :old_yang }
     hexagram = []
     6.times do
-      seed = Time.now.to_f
-      throw = marbles[Random.new(seed).random_number(0..15)]
+      throw = marbles[Random.new.random_number(15)]
       hexagram << throw
     end
     hexagram.map { |e| MAP[e] }
