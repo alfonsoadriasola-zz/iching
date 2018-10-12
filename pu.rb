363c194908891346ce0205ac1db98e0431ff8345
Alfonso Adriasola <aadriasola@cloudpassage.com>
2018-10-12 14:11:40 -0700

    lets encrypt

--- pu.rb
+++ pu.rb
@@ -1,5 +1,9 @@
 # pu.rb
 require 'sinatra'
+require 'openssl'
+require 'webrick'
+require 'webrick/https'
+
 ENV['WEB'] = '1'
 get '/' do
   erb :index
@@ -10,3 +14,12 @@ get '/cast/' do
   markdown Iching.new.cast(renderer: StringHexagramRenderer),
            layout_engine: :erb
 end
+
+webrick_options = {
+  :Port => 8443,
+  :Logger => WEBrick::Log::new($stdout, WEBrick::Log::DEBUG),
+  :SSLEnable => true,
+  :SSLCertificate => OpenSSL::X509::Certificate.new(File.open("./cert.crt").read),
+  :SSLPrivateKey => OpenSSL::PKey::RSA.new(File.open("./pkey.pem").read),
+  :SSLCertName => [['CN', 'localhost', OpenSSL::ASN1::PRINTABLESTRING]]
+}
