112bf0e87707425f1801e0702da5952fc032ab34
Alfonso Adriasola <aadriasola@cloudpassage.com>
2018-10-12 14:24:20 -0700

    Revert "lets encrypt"
    This reverts commit 363c194908891346ce0205ac1db98e0431ff8345.

--- pu.rb
+++ pu.rb
@@ -1,9 +1,5 @@
 # pu.rb
 require 'sinatra'
-require 'openssl'
-require 'webrick'
-require 'webrick/https'
-
 ENV['WEB'] = '1'
 get '/' do
   erb :index
@@ -14,12 +10,3 @@ get '/cast/' do
   markdown Iching.new.cast(renderer: StringHexagramRenderer),
            layout_engine: :erb
 end
-
-webrick_options = {
-  :Port => 8443,
-  :Logger => WEBrick::Log::new($stdout, WEBrick::Log::DEBUG),
-  :SSLEnable => true,
-  :SSLCertificate => OpenSSL::X509::Certificate.new(File.open("./cert.crt").read),
-  :SSLPrivateKey => OpenSSL::PKey::RSA.new(File.open("./pkey.pem").read),
-  :SSLCertName => [['CN', 'localhost', OpenSSL::ASN1::PRINTABLESTRING]]
-}
